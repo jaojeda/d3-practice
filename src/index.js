0f1ca6546e74cc792bb54fad530d18fb47c11da1
Jose <j.a.ojeda@live.com>
2020-09-22 23:17:05 -0700

    set up face

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import * as d3 from 'd3';
+import { select } from 'd3';
 
-const square = d3.selectAll("rect");
-square.style("fill", "orange");
+console.log('test');
+const svg = select('svg');
+
+const width = +svg.attr('width');
+console.log(width);
+const height = +svg.attr('height');
+
+const circle = svg.append('circle')
+  .attr('r', height / 2)
+  .attr('cx', width / 2)
+  .attr('cy', height / 2)
+  .attr('fill', 'yellow')
+  .attr('stroke', 'black')
